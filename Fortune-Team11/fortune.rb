6c66eb64bcf68178a8c9e10e1041abea4ea882b3
Greg <lapresga@rose-hulman.edu>
2011-05-07 17:35:00 -0400

    task 5 all tests passed

--- Fortune-Team11/fortune.rb
+++ Fortune-Team11/fortune.rb
@@ -3,5 +3,10 @@ require 'haml'
 require 'thin'
 
 class Fortune < Sinatra::Base
-
+get '/fortune/random/?' do
+@fortune = $fortunes[rand($fortunes.size)]
+haml :fortune
 end
+end
+
+require 'fortunedata'
